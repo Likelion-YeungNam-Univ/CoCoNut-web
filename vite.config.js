23ac7572de8c0fb66d6f147cf8f21a7d41cb56cd
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-07 19:18:26 +0900

    feat : CI workflow 생성

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    open: true,
+    host: true, // 외부에서 접근 가능하게 설정
+    port: 5173,
   },
 });
