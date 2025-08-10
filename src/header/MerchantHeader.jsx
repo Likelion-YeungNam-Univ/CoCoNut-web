91b75bf3044944bb12b8e1f1cce95e5b6116bb05
hyeji-neee <tlsgmlsk123@naver.com>
2025-08-10 19:30:24 +0900

    style : 소상공인 헤더 스타일 적용

--- src/header/MerchantHeader.jsx
+++ src/header/MerchantHeader.jsx
@@ -1,40 +1,46 @@
 import React from "react";
+import logo from "../assets/logo.png";
 import { BiSearch } from "react-icons/bi";
+import { IoPersonCircle } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const MerchantHeader = () => {
   return (
-    <header className="w-full h-[60px] flex justify-between items-center px-28 py-4 border-b border-gray-200">
-      {/* 왼쪽: 로고 */}
-      <div className="text-2xl font-bold text-[#5f5f5f] cursor-pointer">
-        Bridgee
+    <div className="w-full h-[60px] flex items-center px-[120px] border-b border-gray-200 font-pretendard">
+      {/* 로고 */}
+      <div className="flex-1 flex justify-start">
+        <img src={logo} alt="로고" className="w-[89.65px] h-[20px]" />
       </div>
 
-      {/* 오른쪽: 검색창 + 버튼들 */}
-      <div className="flex items-center gap-9">
-        {/* 검색창 */}
-        <div className="flex items-center w-[240px] h-[32px] bg-[#EDEDED] rounded-full px-3">
-          <BiSearch className="text-[#B9B9B9] w-[12px] h-[12px] ml-1" />
+      {/* 검색창 */}
+      <div className="flex-1 flex justify-center">
+        <div
+          className="flex items-center w-[480px] h-[36px] bg-[#F3F3F3] border-[1px] border-transparent
+      focus-within:bg-[#FBFBFB] focus-within:border-[#2FD8F6] rounded-[18px] px-3"
+        >
+          <BiSearch className="text-[#A3A3A3] w-[14px] h-[14px] ml-1" />
           <input
             type="text"
             placeholder="어떤 공모전을 찾고있나요?"
-            className="ml-1.5 bg-[#EDEDED] placeholder:text-[12px] placeholder:text-[#B9B9B9] text-[12px] w-full focus:outline-none"
+            className="ml-1.5 bg-[#F3F3F3] text-[12px] text-[#212121] placeholder:text-[12px] placeholder:text-[#A3A3A3] placeholder:font-medium focus:bg-[#FBFBFB] focus:outline-none"
           />
         </div>
+      </div>
 
-        {/* 버튼: 내 공모전 */}
-        <button className="text-[12px] text-black font-medium hover:underline cursor-pointer">
+      {/* 버튼들 */}
+      <div className="flex-1 flex justify-end gap-8">
+        <button className="text-[12px] text-[#4C4C4C] font-medium cursor-pointer">
           내 공모전
         </button>
-
-        {/* 버튼: 공모전 등록 */}
-        <button className="w-[107px] h-[32px] text-black font-medium border-[1px] rounded-[4px] text-[12px] cursor-pointer">
+        <Link
+          to="/project-register"
+          className="w-[106px] h-[32px] px-[14px] py-[7px] text-[#4C4C4C] font-medium border-[1px] border-[#4C4C4C] rounded-[6px] text-[12px] cursor-pointer"
+        >
           공모전 등록하기
-        </button>
-
-        {/* 프로필 */}
-        <div className="w-9 h-9 rounded-full bg-[#D9D9D9] cursor-pointer" />
+        </Link>
+        <IoPersonCircle className="text-[#B9B9B9] w-[32px] h-[32px] cursor-pointer" />
       </div>
-    </header>
+    </div>
   );
 };
 
