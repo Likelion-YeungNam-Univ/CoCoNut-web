9e58213f8290ac02d0ce6b79ca9e26e185bd3367
chominseo <singer100@yu.ac.kr>
2025-08-13 00:26:08 +0900

    fix: rending 페이지 수정

--- src/pages/Rending.jsx
+++ src/pages/Rending.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png';
+
 
 const Rending = () => {
   return (
     <>
-    <div className='border h-[60px] border-t-[#FFFFFF] border-b-[#F3F3F3] '>
-       <div className='flex flex-row justify-between px-[120px] py-[20px]'>
-        <img src={logo} alt="로고" className="w-[89.65px] h-[20px]" />
-        <div className='flex  font-pretendard gap-3'>
-        <Link className='border bg-[#FFFFFF] text-[#000000] hover:bg-[#F3F3F3]  rounded-[6px] border-[#FFFFFF] w-[73px] h-[32px] flex justify-center items-center'to='/signin'>로그인</Link>
-        <Link className='border bg-[#4C4C4C] hover:bg-[#212121] text-[#FFFFFF] rounded-[6px] w-[73px] h-[32px] flex justify-center items-center ' to='/signup'>회원가입</Link>
 
-        </div>
-    </div>
-    </div>
     <div className='flex flex-col items-center gap-5 mt-40 font-pretendard'>
     <span className='font-bold text-4xl'>가게에 필요한 것,쉽게 요청하고 받아보세요.</span>
     <span className='text-[#626262]'>브릿지는 경산의 소상공인과 지역 주민이 연결될 수 있도록 돕는 B2P 공모 플랫폼입니다.</span>
