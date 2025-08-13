ed5b7cfeccc89da3c417566f126d97d91dc7d8b8
chominseo <singer100@yu.ac.kr>
2025-08-13 17:49:03 +0900

    feat: 인증 및 로그인 회원가입 API 모듈 추가

---
+++ src/apis/api.jsx
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://13.209.41.51:8080/api/v1/",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+})
+
+export default api;
