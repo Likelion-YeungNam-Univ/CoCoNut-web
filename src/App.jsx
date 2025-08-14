078ff381c23bf1dd3eff09dafef5fdcacdab0443
hyeji-neee <tlsgmlsk123@naver.com>
2025-08-14 19:04:45 +0900

    feat : 참가자 메인 페이지 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,33 @@ import SearchPage from "./pages/SearchPage";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import AuthProvider from "./contexts/AuthProvider";
+import ParticipantMainPage from "./pages/ParticipantMainPage";
 
 export default function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Rending />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="signin" element={<Signin />} />
-        </Route>
-        <Route path="/merchant-main-page" element={<MerchantMainPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Rending />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="signin" element={<Signin />} />
+          </Route>
+          <Route path="/merchant-main-page" element={<MerchantMainPage />} />
+          <Route
+            path="/participant-main-page"
+            element={<ParticipantMainPage />}
+          />
+          <Route
+            path="/search"
+            element={<SearchPage showRegisterButton={true} />}
+          />
+          <Route
+            path="/participant-search"
+            element={<SearchPage showRegisterButton={false} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
---
+++ src/pages/ParticipantMainPage.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ProjectStatusTabs from "../main/ProjectStatusTabs";
+import CategoryFilter from "../main/CategoryFilter";
+import ProjectList from "../main/ProjectList";
+import Footer from "../components/Footer";
+import ParticipantHeader from "../header/ParticipantHeader";
+import ParticipantBanner from "../main/ParticipantBanner";
+
+const ParticipantMainPage = () => {
+  const [activeTab, setActiveTab] = useState("IN_PROGRESS"); // 진행중, 투표중, 완료 탭 상태 / 참가자 기본값 : 진행중
+  const [selectedCategories, setSelectedCategories] = useState([]); // 카테고리 선택 상태
+  const [selectedBusinesses, setSelectedBusinesses] = useState([]); // 업종 선택 상태
+  const [q, setQ] = useState(""); // 검색어 상태
+  const [isSearched, setIsSearched] = useState(false); // 검색 제출 여부
+  const [categories, setCategories] = useState([]); // 카테고리 API 상태
+  const [businessTypes, setBusinessTypes] = useState([]); // 업종 목록 상태
+
+  return (
+    <div>
+      <ParticipantHeader />
+      <ParticipantBanner />
+      <ProjectStatusTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div className="flex px-[240px] gap-[40px]">
+        <CategoryFilter
+          categories={categories}
+          setCategories={setCategories}
+          businessTypes={businessTypes}
+          setBusinessTypes={setBusinessTypes}
+          selectedCategories={selectedCategories}
+          setSelectedCategories={setSelectedCategories}
+          selectedBusinesses={selectedBusinesses}
+          setSelectedBusinesses={setSelectedBusinesses}
+        />
+        <ProjectList
+          q={q}
+          isSearched={isSearched}
+          activeTab={activeTab}
+          categories={categories}
+          businessTypes={businessTypes}
+          selectedCategories={selectedCategories}
+          selectedBusinesses={selectedBusinesses}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ParticipantMainPage;
