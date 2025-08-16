e7e5214326c0d8d6cb3ed42b98f7ead2512236fd
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-17 05:04:49 +0900

    style : 약관 자세히 보기 모달 스타일 수정

--- src/components/TermsModal.jsx
+++ src/components/TermsModal.jsx
@@ -5,9 +5,13 @@ const TermsModal = ({ isOpen, onClose, title, content }) => {
 
   return (
     <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center font-pretendard">
-      <div className="bg-white rounded-lg shadow-xl p-8 w-full max-w-xl max-h-[80vh] overflow-y-auto relative">
+      {/* 모달 컨테이너의 flex-col을 추가하여 내부 콘텐츠의 높이가 늘어나도록 설정 */}
+      <div className="bg-white rounded-lg shadow-xl p-8 w-full max-w-3xl h-[750px] overflow-y-auto relative flex flex-col">
         <h1 className="text-xl text-center font-bold mb-4">{title}</h1>
-        <pre className="text-gray-700 whitespace-pre-wrap">{content}</pre>
+        {/* 'content' 영역이 유연하게 늘어나도록 flex-grow를 추가 */}
+        <pre className="text-gray-700 whitespace-pre-wrap flex-grow">
+          {content}
+        </pre>
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 text-2xl font-semibold"
