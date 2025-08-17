accdb0262cd30eb6be538d16670dd9811481ed25
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-18 02:33:22 +0900

    style : 주석 내용 제거

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -5,9 +5,7 @@ const ConfirmModal = ({ isOpen, onClose, onConfirm }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-start justify-center bg-black/80 p-18 font-pretendard">
-      {/* 모달 콘텐츠를 Flexbox 컨테이너로 만들어 상하 정렬 */}
       <div className="bg-white rounded-xl max-w-md w-full m-4 mt-10 p-4 flex flex-col justify-between min-h-[200px]">
-        {/* 텍스트를 왼쪽 정렬 */}
         <div className="space-y-2 mt-4 ml-5">
           <h1 className="text-[20px] font-semibold text-[#212121] text-left">
             공모전을 등록하시겠습니까?
@@ -16,7 +14,6 @@ const ConfirmModal = ({ isOpen, onClose, onConfirm }) => {
             등록 이후에는 수정할 수 없습니다. 꼭 다시 한 번 확인해주세요.
           </h1>
         </div>
-        {/* 버튼을 오른쪽 하단에 배치 */}
         <div className="flex justify-end gap-4 mb-3 mr-3">
           <button
             onClick={onClose}
