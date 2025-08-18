773afbc9dcfdcf8b153b67b4eaf03d262af54311
chominseo <singer100@yu.ac.kr>
2025-08-18 19:33:14 +0900

    fix: baseURL 수정

--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.209.41.51:8080/api/v1/",
+  baseURL: "http://15.165.164.49:8080/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
