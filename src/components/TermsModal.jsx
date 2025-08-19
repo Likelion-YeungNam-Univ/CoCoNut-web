25943651e6942570dccce737f426bdd59c07a1ba
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-20 04:12:53 +0900

    style : 약관 모달 스타일 변경

--- src/components/TermsModal.jsx
+++ src/components/TermsModal.jsx
@@ -1,20 +1,57 @@
 import React from "react";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const TermsModal = ({ isOpen, onClose, title, content }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center font-pretendard">
-      <div className="bg-white rounded-lg shadow-xl p-8 w-full max-w-3xl h-[750px] overflow-y-auto relative flex flex-col">
-        <h1 className="text-xl text-center font-bold mb-4">{title}</h1>
-        <pre className="text-gray-700 whitespace-pre-wrap flex-grow">
-          {content}
-        </pre>
+    <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center font-pretendard p-4">
+      <div className="flex items-start">
+        <div className="bg-white rounded-lg shadow-xl w-[840px] h-[650px] overflow-hidden flex flex-col relative">
+          <div className="p-12 pb-4 flex-shrink-0">
+            <h1 className="text-[16px] font-semibold text-[#212121] text-left">
+              {title}
+            </h1>
+          </div>
+          <div className="px-12 mt-3 flex-grow overflow-y-auto">
+            {content.items ? (
+              <div>
+                {content.items.map((item, index) => (
+                  <div key={index} className="mb-4">
+                    <h3 className="font-semibold text-[12px] text-base text-[#212121]">
+                      {item.step}
+                    </h3>
+                    <p className="font-normal text-[12px] text-[#212121] mt-2">
+                      {item.description}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : content.sections ? (
+              <div>
+                {content.sections.map((section, index) => (
+                  <div key={index} className="mb-6">
+                    <h3 className="font-semibold text-[12px] text-base text-[#212121]">
+                      {section.subtitle}
+                    </h3>
+                    <ul className="list-inside mt-2 text-[12px] text-[#212121] space-y-1">
+                      {section.points.map((point, pointIndex) => (
+                        <li key={pointIndex}>- {point}</li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>{content}</p>
+            )}
+          </div>
+        </div>
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 text-2xl font-semibold"
+          className="ml-4 flex-shrink-0 text-white transition hover:text-[#A3A3A3]"
         >
-          &times;
+          <IoIosCloseCircle size={40} />
         </button>
       </div>
     </div>
