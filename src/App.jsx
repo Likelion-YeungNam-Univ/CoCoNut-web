d2ae8ddd9d5d56d3ecce8ab4aad301c4f8c00247
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-21 05:42:46 +0900

    feat : 참여자 마이페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ProjectSubmissionPage from "./pages/ProjectSubmissionPage";
 import MerchantMyProject from "./pages/MerchantMyProject.jsx";
 import ParticipantMyProject from "./pages/ParticipantMyProject.jsx";
 import GuestMainPage from "./pages/GuestMainPage.jsx";
+import ParticipantMyPage from "./pages/ParticipantMyPage.jsx"; // ParticipantMyPage 컴포넌트 import
 
 export default function App() {
   return (
@@ -52,6 +53,7 @@ export default function App() {
             path="participant-myproject"
             element={<ParticipantMyProject />}
           />
+          <Route path="participant-mypage" element={<ParticipantMyPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
