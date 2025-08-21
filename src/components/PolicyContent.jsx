c58219ef27692ea6639d69d51fea0e627c6e8af2
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-08-21 14:31:35 +0900

    feat : 회원가입 약관 추가

--- src/components/PolicyContent.jsx
+++ src/components/PolicyContent.jsx
@@ -1,37 +1,50 @@
 import React from "react";
-import { TERMS_DATA } from "../termsData";
+import { TERMS_DATA } from "../utils/termsData";
 
 const PolicyContent = ({ type }) => {
   let data;
+  let ListComponent;
+  let listStyleClass;
 
   switch (type) {
     case "terms":
-      data = TERMS_DATA.terms;
+      data = TERMS_DATA.serviceTerms;
+      ListComponent = "ol";
+      listStyleClass = "list-decimal pl-5";
       break;
     case "privacy":
       data = TERMS_DATA.personalInfo;
+      ListComponent = "ul";
+      listStyleClass = "list-none";
       break;
     default:
       return null;
   }
+
   if (!data || !data.sections) {
     return (
       <div className="text-gray-700 p-5">약관 정보를 불러올 수 없습니다.</div>
     );
   }
 
+  const isNumbered = (text) => /^\(\d+\)/.test(text.trim());
+
   return (
-    <div className="pl-10 pr-20 pb-10 text-[14px] font-pretendard">
+    <div className="pr-20 pb-10 text-[14px] font-pretendard">
       {data.sections.map((section, index) => (
         <div key={index} className="mb-6">
-          <h3 className="font-bold mb-2 text-lg">{section.subtitle}</h3>
-          <ul className="list-none p-0">
+          <h3 className="font-semibold mb-2 text-[12px]">{section.subtitle}</h3>
+          <ListComponent className={listStyleClass}>
             {section.points.map((point, pointIndex) => (
-              <li key={pointIndex} className="mb-1 text-[#4c4c4c]">
+              <li
+                key={pointIndex}
+                className="mb-1 text-[#212121] font-normal text-[12px]"
+              >
+                {type === "privacy" && !isNumbered(point) && "- "}
                 {point}
               </li>
             ))}
-          </ul>
+          </ListComponent>
         </div>
       ))}
     </div>
