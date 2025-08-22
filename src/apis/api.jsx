d95c7ac859ffcf536385649aa0b412fb0dc4feea
chominseo <singer100@yu.ac.kr>
2025-08-22 21:14:44 +0900

    fix: 충돌해결

--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://15.165.164.49:8080/api/v1/",
-
-    withCredentials: true,    
+  withCredentials: true,
 });
+
+
 
 api.interceptors.request.use(
   (config) => {
